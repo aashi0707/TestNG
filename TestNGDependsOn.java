00f558fe6af6ca2e01fbe170fb0f4aeb55a6f2af
Aashi <45652647+aashi0707@users.noreply.github.com>
2024-05-24 15:56:26 +0530

    dependsOnMethods

---
+++ TestNGDependsOn.java
@@ -0,0 +1,25 @@
+package Framework;
+
+import org.testng.annotations.Test;
+
+public class TestNGDependsOn {
+
+	@Test(priority = 2)
+	public void createContact()
+	{
+		
+		System.out.println("Contact created");
+	}
+
+	@Test(dependsOnMethods = "createContact")
+	public void modifyContact()
+	{
+		System.out.println("Contact modified");
+	}
+	
+	@Test(dependsOnMethods = "createContact")
+	public void deleteContact() 
+	{
+		System.out.println("Contact Deleted");
+	}
+}
