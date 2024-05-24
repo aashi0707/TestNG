5b8bdd1cd1c7364b5ba8e189fe7849a6e718e2d9
Aashi <45652647+aashi0707@users.noreply.github.com>
2024-05-24 15:54:51 +0530

    use of @Test

---
+++ TestNGImplementation.java
@@ -0,0 +1,18 @@
+package Framework;
+
+import org.testng.annotations.Test;
+
+public class TestNGImplementation {
+	
+	@Test
+	public void createContact()
+	{
+		System.out.println("Contact created");
+	}
+
+	@Test
+	public void modifyContact()
+	{
+		System.out.println("Contact modified");
+	}
+}
